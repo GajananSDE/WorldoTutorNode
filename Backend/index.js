49da5717915cec1143997aa4965f96ee41f857ce
GajananSDE <gajananprajapati2002@gmail.com>
2024-05-24 18:46:51 +0530

    React host page

--- Backend/index.js
+++ Backend/index.js
@@ -9,17 +9,16 @@ require('./Mongoose/config')
 const crypto = require('crypto')
 
 const app = express();
-app.use(express.static(path.join(__dirname,'../SubectEnrollment/dist')))
+app.use(express.static(path.join(__dirname,'./Public_html')))
 app.use(cors())
 app.use(express.json())
 app.use("/api/payment",Payment)
 app.use("/api/course",Cource)
 app.use("/api/formsdetail",Forms)
 app.get("/*",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../SubectEnrollment/dist','index.html'))
+    res.sendFile(path.join(__dirname,'./Public_html','index.html'))
 })
 
 app.listen(process.env.PORT,()=>{
     console.log(`Server Running on ${process.env.PORT}`)
 })
-    
